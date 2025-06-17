f2a3587a0bd99fea8eef5c4a71f0033420fca465
Ely <martharun514@gmail.com>
2025-06-18 00:54:50 +0100

    feat: Implement active status fetching and loading state in App component; add Loading page

--- you_go_app/frontend/src/pages/LandingPage.jsx
+++ you_go_app/frontend/src/pages/LandingPage.jsx
@@ -2,16 +2,14 @@ import Navbar from '../components/Navbar';
 import Button from "../components/Button";
 import { useState } from 'react';
 function Landing() {
-    const [anim, setanim] = useState('animate-ping')
     const [trans, setTrans] = useState()
     const [logo, setlogo] = useState('./logo.svg')
     setTimeout(() => {
-        setanim('hidden')
         setTrans('-translate-y-[45vh] -translate-x-[40vw] scale-28')
-    }, 3000);
+    }, 500);
     setTimeout(() => {
         setlogo('./logo-light.svg')
-    }, 3200);
+    }, 700);
 
     // bg-[url(./src/assets/img/bg.jpg)]
     // bg-white
@@ -23,31 +21,24 @@ function Landing() {
                     className={`z-10 w-2/6 absolute ${trans} transition-all duration-500`}
                     src={logo}
                     alt="" />
-                <img
-                    className={`w-2/8 ${anim} absolute opacity-60 animate-duration-2000`}
-                    src="/logo.svg"
-                    alt="" />
-                <img
-                    className={`w-2/9 ${anim} absolute opacity-60 animate-duration-2000`}
-                    src="/logo.svg"
-                    alt="" />
+
                 <div className=' w-full h-screen flex flex-col items-center justify-center gap-6'>
                     <div className='w-full'>
                         <h1
-                            className='w-full px-14 text-4xl text-gray-600 font-bold animate-fade-right animate-delay-3500'
+                            className='w-full px-14 text-4xl text-gray-600 font-bold animate-fade-right animate-delay-1000'
                         >Bienvenue <span className='animate-fade animate-delay-200'>sur</span></h1>
                         <h1
-                            className='w-full text-center my-2 text-[#ffcb74] text-4xl font-bold animate-fade-right animate-delay-3800'
+                            className='w-full text-center my-2 text-[#ffcb74] text-4xl font-bold animate-fade-right animate-delay-1300'
                         >YouGo App</h1>
                     </div>
 
                     <img
-                        className='max-w-lg w-[30vh] animate-fade animate-ease-in-out animate-delay-4000'
+                        className='max-w-lg w-[30vh] animate-fade animate-ease-in-out animate-delay-1500'
                         src="./src/assets/img/carpool.gif"
                         alt="Login" />
-                    <h1 className='w-8/10 text-xl text-center font-semibold mx-auto animate-fade animate-delay-4000'>L'application de covoiturage pour Etudiants</h1>
+                    <h1 className='w-8/10 text-xl text-center font-semibold mx-auto animate-fade animate-delay-1500'>L'application de covoiturage pour Etudiants</h1>
                 </div>
-                <div className='w-full flex flex-col items-center animate-fade animate-delay-4200'>
+                <div className='w-full flex flex-col items-center animate-fade animate-delay-1700'>
                     <Button text={'Creer un compte'} textCol={'text-white'} bg={'bg-[#ffcb74]'} submitted={true} link={'/Register'} />
                     <p
                         className="my-5 text-gray-600">
